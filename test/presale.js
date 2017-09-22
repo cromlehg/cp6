f7aea15f0af9c3fd1d062aadc7ec0f2724777dee
cromlehg <cromlehg@gmail.com>
2017-09-22 08:56:51 +0300

    Last fixes

--- test/presale.js
+++ test/presale.js
@@ -1,196 +1,247 @@
-/*var PreSale = artifacts.require("./PreSale.sol");
 
-contract('PreSale', function(accounts) {
+import ether from './helpers/ether'
+import {advanceBlock} from './helpers/advanceToBlock'
+import {increaseTimeTo, duration} from './helpers/increaseTime'
+import latestTime from './helpers/latestTime'
+import EVMThrow from './helpers/EVMThrow'
+
+const BigNumber = web3.BigNumber
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+
+const PreSale = artifacts.require('PreSale')
+
+contract('PreSale', function(wallets) {
+
+  const owner = wallets[0]
+
+  const notOwner = wallets[1]
+
+  const newOwner = wallets[2]
+
+  const wallet = wallets[3]
+
+  const newWallet = wallets[4]
  
+  const investor1 = wallets[5]
 
-  //==================== investments time tests ========================//
-  //
-  // owner initialized in customer way - changed to customer after deploy
-  // wallet initialized as 2
-  // otjer fields initialized in after deploy state
-  // detailed:
-  //   account 0 - old owner
-  //   account 1 - current owner
-  //   account 2 - wallet
-  //   start - 0 - should change in current tests
-  //   period - 5
-  //   paused = not
-  //
-  //   work account - 3
-  //
+  const investor2 = wallets[6]
 
-  it("investment time test: should not invest before start date", function() {
-    var meta;
-    var startDate = Math.floor(Date.now()/1000) + 60*60;
-    var invested = web3.toWei(15, 'ether');
-    return PreSale.deployed().then(function(instance) {
-      meta = instance;
-      return meta.setStart(startDate, {from: accounts[1]});
-    }).then(function(wallet) {
-      return web3.eth.sendTransaction({ from: accounts[3], to: meta.address, value: invested, gas: 180000 })
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      assert.equal(0, total, "invested");
-    }).catch(function(e) {
-      if(e.toString().indexOf("invalid opcode") != -1) {
-        return meta.total.call().then(function(total) {
-          assert.equal(0, total, "invested");
-        });
-      } else {
-        throw e;
-      }
-    });
-  });
+  const investor3 = wallets[7]
 
-  it("investment time test: should not invest after start date", function() {
-    var meta;
-    var startDate = Math.floor(Date.now()/1000) - 10*24*60*60;
-    var invested = web3.toWei(15, 'ether');
-    return PreSale.deployed().then(function(instance) {
-      meta = instance;
-      return meta.setStart(startDate, {from: accounts[1]});
-    }).then(function(wallet) {
-      return web3.eth.sendTransaction({ from: accounts[3], to: meta.address, value: invested, gas: 180000 })
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      assert.equal(0, total, "invested");
-    }).catch(function(e) {
-      if(e.toString().indexOf("invalid opcode") != -1) {
-        return meta.total.call().then(function(total) {
-          assert.equal(0, total, "invested");
-        });
-      } else {
-        throw e;
-      }
-    });
-  });
+  const investor4 = wallets[8]
 
-  //======================= integration test ==========================================//
-  //
-  // send from account 3 - 15
-  // send from account 4 - 40
-  // send from account 5 - 65
-  //
-  it("Integration test during invest time", function() {
-    var meta;
-    var gasNeeds = 140000;
-    var startDate = Math.floor(Date.now()/1000) - 2*24*60*60;
-    var invested1 = parseInt(web3.toWei(15, 'ether'));
-    var invested2 = parseInt(web3.toWei(40, 'ether'));
-    var invested3 = parseInt(web3.toWei(165, 'ether'));
-    var invested4 = parseInt(web3.toWei(385, 'ether'));
-    var walletBalance;
-    return PreSale.deployed().then(function(instance) {
-      meta = instance;
-      return meta.setStart(startDate, {from: accounts[1]});
-    }).then(function(wallet) {
-      return web3.eth.getBalance(accounts[2]);
-    }).then(function(balance) {
-      walletBalance = parseInt(balance);
-      return web3.eth.sendTransaction({ from: accounts[3], to: meta.address, value: invested1, gas: gasNeeds });
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      return assert.equal(invested1, total, "wrong total field");
-    }).then(function() {
-      return meta.totalInvestors.call();
-    }).then(function(totalInvestors) {
-      return assert.equal(1, totalInvestors, "wrong investors count");
-    }).then(function() {
-      return meta.investors.call(0);
-    }).then(function(investor) {
-      return assert.equal(accounts[3], investor, "wrong investor address");
-    }).then(function() {
-      return meta.balanceOf.call(accounts[3]);
-    }).then(function(balance) {
-      return assert.equal(invested1, balance, "wrong investor balance");
-    }).then(function() {
-      return web3.eth.getBalance(accounts[2]);
-    }).then(function(balance) {
-      console.log("Wallet old balance: " + walletBalance);
-      console.log("Ivested: " + invested1);
-      console.log("Wallet current balance: " + balance);
-      console.log("Wallet current balance should be: " + (walletBalance + invested1));
-      return assert.equal(walletBalance + invested1, balance, "wrong wallet balance");
-    // 2
-    }).then(function() {
-      return web3.eth.sendTransaction({ from: accounts[4], to: meta.address, value: invested2, gas: gasNeeds });
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      return assert.equal(invested1 + invested2, total, "wrong total field after second invest");
-    }).then(function() {
-      return meta.totalInvestors.call();
-    }).then(function(totalInvestors) {
-      return assert.equal(2, totalInvestors, "wrong investors count - should be 2");
-    }).then(function() {
-      return meta.investors.call(1);
-    }).then(function(investor) {
-      return assert.equal(accounts[4], investor, "wrong second investor address");
-    }).then(function() {
-      return meta.balanceOf.call(accounts[4]);
-    }).then(function(balance) {
-      return assert.equal(invested2, balance, "wrong second investor balance");
-    }).then(function() {
-      return web3.eth.getBalance(accounts[2]);
-    }).then(function(balance) {
-      return assert.equal(walletBalance + invested1 + invested2, balance, "wrong wallet balance after second investor");
-    // 3
-    }).then(function() {
-      return web3.eth.sendTransaction({ from: accounts[5], to: meta.address, value: invested3, gas: gasNeeds });
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      return assert.equal(invested1 + invested2 + invested3, total, "wrong total field after thrid invest");
-    }).then(function() {
-      return meta.totalInvestors.call();
-    }).then(function(totalInvestors) {
-      return assert.equal(3, totalInvestors, "wrong investors count - should be 3");
-    }).then(function() {
-      return meta.investors.call(2);
-    }).then(function(investor) {
-      return assert.equal(accounts[5], investor, "wrong thrid investor address");
-    }).then(function() {
-      return meta.balanceOf.call(accounts[5]);
-    }).then(function(balance) {
-      return assert.equal(invested3, balance, "wrong thrid investor balance");
-    }).then(function() {
-      return web3.eth.getBalance(accounts[2]);
-    }).then(function(balance) {
-      return assert.equal(walletBalance + invested1 + invested2 + invested3, balance, "wrong wallet balance after thrid investor");
-    // 3x2
-    }).then(function() {
-      return web3.eth.sendTransaction({ from: accounts[5], to: meta.address, value: invested4, gas: gasNeeds });
-    }).then(function() {
-      return meta.total.call();
-    }).then(function(total) {
-      return assert.equal(invested1 + invested2 + invested3 + invested4, total, "wrong total field after trid twice invest");
-    }).then(function() {
-      return meta.totalInvestors.call();
-    }).then(function(totalInvestors) {
-      return assert.equal(3, totalInvestors, "wrong investors count after twice invest - should be 3");
-    }).then(function() {
-      return meta.balanceOf.call(accounts[5]);
-    }).then(function(balance) {
-      return assert.equal(invested3 + invested4, balance, "wrong thrid investor balance afte twice invest");
-    }).then(function() {
-      return web3.eth.getBalance(accounts[2]);
-    }).then(function(balance) {
+  const period = new BigNumber(60)
 
-      console.log("====================================================================");
-      var invested = invested1 + invested2 + invested3 + invested4;
-      console.log("Wallet old balance: " + walletBalance);
-      console.log("Ivested: " + invested);
-      console.log("Wallet current balance: " + balance);
-      console.log("Wallet current balance should be: " + (walletBalance + invested));
+  const min = new BigNumber(1000000000000000000)
+
+  const start = new BigNumber(1613205200)
+
+  const hardcap = new BigNumber(9000000000000000000)
+
+  const newMin = new BigNumber(1000000000000000000)
+
+  const newPeriod = new BigNumber(70)
+
+  const newStart = new BigNumber(1614505200)
+
+  const newHardcap = new BigNumber(10000000000000000000)
 
 
-      return assert.equal(walletBalance + invested1 + invested2 + invested3 + invested4, balance, "wrong wallet balance after thrid twice invest");
-    });
+  before(async function() {
+   //Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+   await advanceBlock()
+  })
 
-  });
+
+  beforeEach(async function () {
+    this.presale = await PreSale.new()
+  })	 
+
+  describe('invest tests', function () {
+
+    const startDate = new BigNumber(Math.floor(Date.now()/1000) - 10*24*60*60)
+
+    const investor1Value1 = ether(11)
+
+    const investor1Value2 = ether(12)
+
+    const investor2Value1 = ether(13)
+
+    it('should invest balances', async function () {
+      await this.presale.setMin(min, {from: owner})
+      await this.presale.setHardcap(hardcap, {from: owner})
+      await this.presale.setStart(startDate, {from: owner})
+      await this.presale.setPeriod(1000, {from: owner})
+
+      await this.presale.send(investor1Value1, {from: investor2}).should.be.fulfilled
+      await this.presale.send(investor1Value2, {from: investor1}).should.be.fulfilled
+      await this.presale.send(investor2Value1, {from: investor2}).should.be.fulfilled
+      investor1Value1.add(investor1Value2).should.be.bignumber.equal(await this.presale.balanceOf(investor1))
+      investor2Value1.should.be.bignumber.equal(await this.presale.balanceOf(investor2))
+    })
+
+  })
 
 
-});*/
+  describe('owner tests', function () {
+
+    it('should be owner', async function () {
+      owner.should.equal(await this.presale.owner())
+    })	 
+
+    it('should reject transfer ownership if not owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setMin if not owner', async function () {
+      await this.presale.setMin(min, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setWallet if not owner', async function () {
+      await this.presale.setWallet(wallet, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setHardcap if not owner', async function () {
+      await this.presale.setHardcap(hardcap, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+    
+    it('should reject setStart if not owner', async function () {
+      await this.presale.setStart(start, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setPeriod if not owner', async function () {
+      await this.presale.setPeriod(period, {from: notOwner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should changed min if owner', async function () {
+      await this.presale.setMin(min, {from: owner})
+      min.should.be.bignumber.equal(await this.presale.min())
+    })	 
+
+    it('should changed wallet if owner', async function () {
+      await this.presale.setWallet(wallet, {from: owner})
+      wallet.should.equal(await this.presale.wallet())
+    })	 
+
+    it('should changed hardcap if owner', async function () {
+      await this.presale.setHardcap(hardcap, {from: owner})
+      hardcap.should.be.bignumber.equal(await this.presale.hardcap())
+    })	 
+    
+    it('should changed start if owner', async function () {
+      await this.presale.setStart(start, {from: owner})
+      start.should.be.bignumber.equal(await this.presale.start())
+    })	 
+
+    it('should changed period if owner', async function () {
+      await this.presale.setPeriod(period, {from: owner})
+      period.should.be.bignumber.equal(await this.presale.period())
+    })	 
+
+    it('owner should changed if owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      newOwner.should.equal(await this.presale.owner())
+    })
+
+    it('should reject setMin if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setMin(min, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setWallet if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setWallet(newOwner, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject transfer ownership if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.transferOwnership(newOwner, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setHardcap if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setHardcap(hardcap, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+    
+    it('should reject setStart if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setStart(start, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should reject setPeriod if old owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setPeriod(period, {from: owner}).should.be.rejectedWith(EVMThrow)
+    })	 
+
+    it('should changed min if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setMin(newMin, {from: newOwner})
+      newMin.should.be.bignumber.equal(await this.presale.min())
+    })	 
+
+    it('should changed wallet if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setWallet(newWallet, {from: newOwner})
+      newWallet.should.equal(await this.presale.wallet())
+    })	 
+
+    it('should changed hardcap if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setHardcap(newHardcap, {from: newOwner})
+      newHardcap.should.be.bignumber.equal(await this.presale.hardcap())
+    })	 
+    
+    it('should changed start if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setStart(newStart, {from: newOwner})
+      newStart.should.be.bignumber.equal(await this.presale.start())
+    })	 
+
+    it('should changed period if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.setPeriod(newPeriod, {from: newOwner})
+      newPeriod.should.be.bignumber.equal(await this.presale.period())
+    })	 
+
+    it('owner should changed if new owner', async function () {
+      await this.presale.transferOwnership(newOwner, {from: owner})
+      await this.presale.transferOwnership(owner, {from: newOwner})
+      owner.should.equal(await this.presale.owner())
+    })
+
+  })
+
+
+  describe('time tests', function () {
+
+    const startDate = new BigNumber(Math.floor(Date.now()/1000) - 10*24*60*60);
+
+    it('should not invest before start date', async function () {
+      await this.presale.setMin(min, {from: owner})
+      await this.presale.setHardcap(hardcap, {from: owner})
+      await this.presale.setStart(startDate, {from: owner})
+      await this.presale.setPeriod(period, {from: owner})
+      await this.presale.send(11, {from: investor1}).should.be.rejectedWith(EVMThrow)
+    })
+
+    it('should not invest after start date', async function () {
+      await this.presale.setMin(min, {from: owner})
+      await this.presale.setHardcap(hardcap, {from: owner})
+      await this.presale.setStart(startDate, {from: owner})
+      await this.presale.setPeriod(period, {from: owner})
+      await increaseTimeTo(startDate.add(period*24*60*60 + 10*24*60*60))
+      await this.presale.send(11, {from: investor1}).should.be.rejectedWith(EVMThrow)
+    })
+
+  })
+
+
+
+ 
+})
+
